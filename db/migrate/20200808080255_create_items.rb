18c9067573a009f58c35b3e0cc51d85e2c28d508
tsuyatsuya-april <ka5meka5mewith871monme@gmail.com>
2020-08-08 19:16:57 +0900

    レイアウト修正

---
+++ db/migrate/20200808080255_create_items.rb
@@ -0,0 +1,17 @@
+class CreateItems < ActiveRecord::Migration[6.0]
+  def change
+    create_table :items do |t|
+      t.string :name,                    null; false
+      t.text :description,               null; false
+      t.integer :category,               null; false
+      t.integer :state,                  null; false
+      t.integer :pay_state,              null; false
+      t.integer :ship_origin_prefecture, null; false
+      t.integer :ship_prepare,           null; false
+      t.integer :price,                  null; false
+      t.integer :integer,                null; false
+      t.timestamps
+    end
+  end
+end
+
