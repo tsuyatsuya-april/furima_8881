e34d42a1ecec8c0d8ccfaa085759e301f096b204
tsuyatsuya-april <ka5meka5mewith871monme@gmail.com>
2020-08-10 00:06:35 +0900

    新規登録の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+  
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :email, :password, :last_name, :first_name, :last_kana_name, :first_kana_name, :birth])
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,17 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_many :items
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
+  with_options presence: true do
+    validates :nickname 
+    validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角文字を使用してください' }
+    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角文字を使用してください' }
+    validates :last_kana_name, format: { with: /\A[ァ-ヶー－]+\z/, message: '全角カタカナを使用してください' }
+    validates :first_kana_name, format: { with: /\A[ァ-ヶー－]+\z/, message: '全角カタカナを使用してください' }
+    validates :birth
+  end
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,100}+\z/i, message: '半角英数字混合のパスワードにしてください' }
+
 end
