0e7b3852270a549dd627dc376a929a3271e685ec
tsuyatsuya-april <ka5meka5mewith871monme@gmail.com>
2020-08-09 12:40:15 +0900

    ヘッダーにユーザー関係のリンク設定完了

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_09_000628) do
+ActiveRecord::Schema.define(version: 2020_08_09_004404) do
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
