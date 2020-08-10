b63bfd1f828fbc975717d44dd8716d21ff237e1a
tsuyatsuya-april <ka5meka5mewith871monme@gmail.com>
2020-08-10 12:47:40 +0900

    新規登録モデルの単体テスト実施

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,14 +1,15 @@
 FactoryBot.define do
   factory :user do
-    nickname          {Faker::Name.name}
+    nickname          {Faker::Name.initials(number: 2)}
     email             {Faker::Internet.free_email}
-    password = Faker::Internet.password(min_length: 6, mix_case: true)
+    password = "abcdefg8"
     password {password}
     password_confirmation {password}
-    last_name         {Faker::Name.last_name} 
-    first_name        {Faker::Name.first_name} 
-    last_kana_name    {Faker::Name.lastKanaName}
-    first_kana_name   {Faker::Name.firstKanaName}
+    gimei = Gimei.name
+    last_name         {gimei.last.kanji}
+    first_name        {gimei.first.kanji}
+    last_kana_name    {gimei.last.katakana}
+    first_kana_name    {gimei.first.katakana}
     birth             {Faker::Date.in_date_period}
   end
 end
