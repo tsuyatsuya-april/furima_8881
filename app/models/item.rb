691959b12af517f6cf53e74b39e68775b62d1bab
tsuyatsuya-april <ka5meka5mewith871monme@gmail.com>
2020-08-10 23:14:52 +0900

    itemのvalidation設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,17 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :user
   has_one_attached :image
+  belongs_to_active_hash :category
+  belongs_to_active_hash :pay_state
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :ship_prepare
+  belongs_to_active_hash :state
+  validates :name, :description, :category, :state,
+            :pay_state, :ship_origin_prefecture,
+            :ship_prepare, :price, presence: true
+  validates :category, :state, :pay_state, :ship_origin_prefecture,
+            :ship_prepares, numericality: { other_than: 1 }
+  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
+
 end
