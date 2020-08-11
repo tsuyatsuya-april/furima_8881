1ceaa53ebfb4b154fd3065415bf14d85595a2e68
tsuyatsuya-april <ka5meka5mewith871monme@gmail.com>
2020-08-11 17:32:20 +0900

    categoryモデルの値変更

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,8 +10,9 @@ class Item < ApplicationRecord
   validates :name, :image, :description, :category_id, :state_id,
             :pay_state_id, :ship_origin_prefecture_id,
             :ship_prepare_id, :price, presence: true
-  validates :category_id, :state_id, :pay_state_id, :ship_origin_prefecture_id,
+  validates :category_id, :state_id, :pay_state_id, 
             :ship_prepare_id, numericality: { other_than: 1 }
+  validates :ship_origin_prefecture_id, numericality: { other_than: 0 }          
   validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
 
 end
