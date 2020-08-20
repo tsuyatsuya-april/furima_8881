984f52ef9d6b0552be5abbac22136cf5037dda01
tsuyatsuya-april <ka5meka5mewith871monme@gmail.com>
2020-08-20 16:07:29 +0900

    購入画面の遷移設定

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,14 +1,15 @@
 class PurchasesController < ApplicationController
-  def index
-  end
 
   def new
+    @item = Item.find(params[:item_id])
     @purchase = PurchaseAddress.new
   end
 
   def create
+    @item = Item.find(params[:item_id])
     @purchase = PurchaseAddress.new(purchase_params)
     if @purchase.valid?
+      pay_item
       @purchase.save
       return redirect_to root_path
     else
@@ -16,10 +17,22 @@ class PurchasesController < ApplicationController
     end
   end
 
+  
+
   private
 
   def purchase_params
-    params.require(:purchase_address).permit(:postalcode, :)
+    params.require(:purchase_address).permit(:postalcode, :street, :city, :prefecture_id, :building, :street, :phone_number, :token)
+  end
+
+  def pay_item
+    @item = Item.find(params[:item_id])
+    Payjp.api_key = "sk_test_"
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: purchase_params[:token],
+      currency: 'jpy'
+    )
   end
 
 end
