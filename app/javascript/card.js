691034e68a5103e3d0bb2ea40c39f012d652ebbd
tsuyatsuya-april <ka5meka5mewith871monme@gmail.com>
2020-08-26 11:34:15 +0900

    クレジット機能javascritの調整

---
+++ app/javascript/card.js
@@ -0,0 +1,46 @@
+const pay = () => {
+  Payjp.setPublicKey(process.env.PAYJP_PUBLIC_KEY);　// PAY.JPテスト公開鍵
+  const form = document.getElementById('charge-form');
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const formResult = document.getElementById('charge-form');
+    const formData = new FormData(formResult);
+    const card = {
+      number: formData.get("purchase_address[number]"),
+      cvc: formData.get("purchase_address[cvc]"),
+      exp_month: formData.get("purchase_address[exp_month]"),
+      exp_year: `20${formData.get("purchase_address[exp_year]")}`,
+    };
+    console.log(card);
+    Payjp.createToken(card, (status, response) => {
+      console.log(status)
+      if (status === 200) {
+        const token = response.id;
+        const renderDom = document.getElementById("charge-form");
+        const tokenObj = `<input value=${token} type="hidden" name='token'>`;
+        renderDom.insertAdjacentHTML("beforeend", tokenObj);
+
+        document.getElementById("card-number").removeAttribute("name");
+        document.getElementById("card-cvc").removeAttribute("name");
+        document.getElementById("card-exp-month").removeAttribute("name");
+        document.getElementById("card-exp-year").removeAttribute("name");
+
+        document.getElementById("charge-form").submit();
+        document.getElementById("charge-form").reset();
+      } else {
+        
+
+        document.getElementById("card-number").removeAttribute("name");
+        document.getElementById("card-cvc").removeAttribute("name");
+        document.getElementById("card-exp-month").removeAttribute("name");
+        document.getElementById("card-exp-year").removeAttribute("name");
+
+        document.getElementById("charge-form").submit();
+        document.getElementById("charge-form").reset();
+      }
+    });
+  });
+};
+
+window.addEventListener("load", pay);
