fdd036986b8f8595a92423a1a148edd265c26bc1
tsuyatsuya-april <ka5meka5mewith871monme@gmail.com>
2020-08-26 11:36:03 +0900

    金額自動出力の編集

--- app/javascript/price.js
+++ app/javascript/price.js
@@ -1,24 +1,15 @@
-$(document).on('turbolinks:load',  function() {
-  // 再度読み込んだ際の処理
+function price() {
   const regex = /^[0-9]{3,7}$/;
-  let $input = $('#item-price');
-  const first = $('#item-price').val();
-  if (regex.test(first)){
-    $('#add-tax-price').text(Math.floor(first / 10).toLocaleString());
-    $('#profit').text(Math.floor(first * 9 / 10).toLocaleString());
-  }
-  // 入力値が変化する度に行われる処理
-  $input.on('input', function(event){
-    let price = $('#item-price').val();
-    let fee = "";
-    let profit = "";
-    if (regex.test(price)){
-      // 小数点以下切り捨て
-      fee = Math.floor(price / 10);
-      profit = Math.floor(price - fee);
-    }
-    $('#add-tax-price').text(fee.toLocaleString());
-    $('#profit').text(profit.toLocaleString());
-  });
-  
-});
+  const price = document.getElementById("item-price");
+  const tax = document.getElementById("add-tax-price");
+  const profit = document.getElementById("profit");
+  price.addEventListener("input", value);
+  function value(e){
+    if (regex.test(price.value)){
+      tax.textContent = Math.floor((e.target.value)*0.1).toLocaleString();
+      profit.textContent = Math.floor((e.target.value)*0.9).toLocaleString();
+    };
+  };
+};
+
+window.addEventListener("load", price);
