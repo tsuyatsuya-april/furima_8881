e69ee808ecdf1fdc99c2d128888ede2ed150f58e
tsuyatsuya-april <ka5meka5mewith871monme@gmail.com>
2020-08-26 19:58:21 +0900

    商品購入機能のバリデーションの修正

--- app/forms/purchase_address.rb
+++ app/forms/purchase_address.rb
@@ -1,15 +1,15 @@
 class PurchaseAddress
-
   include ActiveModel::Model
   attr_accessor :postalcode, :prefecture_id, :city, :street, :building, :phone_number, :user_id, :item_id, :token
   post = /\A[0-9]{3}-[0-9]{4}\z/
+  phone = /\A[0-9]+\z/
   with_options presence: true do
-    validates :postalcode, format: {with: post, message: "is invalid. Include hyphen(-)"}
+    validates :postalcode, format: { with: post, message: 'is invalid. Include hyphen(-)' }
     validates :city
     validates :street
-    validates :phone_number
+    validates :phone_number, format: { with: phone, message: 'is invalid. Please only half size number' }
   end
-  validates :prefecture_id, numericality: { other_than: 0, message: "Select" }
+  validates :prefecture_id, numericality: { other_than: 0, message: 'select' }
 
   def save
     purchase = Purchase.create(user_id: user_id, item_id: item_id)
