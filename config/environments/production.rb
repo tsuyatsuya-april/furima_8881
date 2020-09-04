6d5b1d509d2b5f2da21d73a5b3cc33dd73502b63
tsuyatsuya-april <ka5meka5mewith871monme@gmail.com>
2020-09-04 14:27:46 +0900

    logger削除

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,16 +74,16 @@ Rails.application.configure do
   config.active_support.deprecation = :notify
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
-  config.log_formatter = ::Logger::Formatter.new
+  config.log_formatter = ::::Formatter.new
 
-  # Use a different logger for distributed setups.
-  # require 'syslog/logger'
-  # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
+  # Use a different  for distributed setups.
+  # require 'syslog/'
+  # config. = ActiveSupport::TaggedLogging.new(Syslog::.new 'app-name')
 
   if ENV["RAILS_LOG_TO_STDOUT"].present?
-    logger           = ActiveSupport::Logger.new(STDOUT)
-    logger.formatter = config.log_formatter
-    config.logger    = ActiveSupport::TaggedLogging.new(logger)
+               = ActiveSupport::.new(STDOUT)
+    .formatter = config.log_formatter
+    config.    = ActiveSupport::TaggedLogging.new()
   end
 
   # Do not dump schema after migrations.
